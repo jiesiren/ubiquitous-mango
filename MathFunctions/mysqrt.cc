10fc2a139332957bbd1ea4a0ca8664d25a76725a
Jiesi Ren <jiesiren@gmail.com>
2019-08-10 14:24:11 -0400

    add library included optionally

---
+++ MathFunctions/mysqrt.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <math.h>
+
+double mysqrt(double x) {
+  std::cout << "mysqrt!" << std::endl;
+  return sqrt(x);
+}
--- tutorial.cc
+++ tutorial.cc
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include "TutorialConfig.h"
- 
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
+
 int main (int argc, char *argv[])
 {
   if (argc < 2)
@@ -16,7 +20,13 @@ int main (int argc, char *argv[])
     return 1;
     }
   double inputValue = atof(argv[1]);
+
+#ifdef USE_MYMATH
+  double outputValue = mysqrt(inputValue);
+#else
   double outputValue = sqrt(inputValue);
+#endif
+
   fprintf(stdout,"The square root of %g is %g\n",
           inputValue, outputValue);
   return 0;
